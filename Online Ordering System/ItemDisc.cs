1ec4bd67cd2ffe006022da9c80d678c1a19e90c4
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2017-06-04 17:50:43 +0200

    +Added Interface IComparable for ItemDisc

--- Online Ordering System/ItemDisc.cs
+++ Online Ordering System/ItemDisc.cs
@@ -1,6 +1,16 @@
-﻿public struct ItemDisc
+﻿using System;
+
+public struct ItemDisc : IComparable
 {
     int ID;
     string name;
     float price;
+    public int CompareTo(object o)
+    {
+        if (o.GetType() == GetType())
+        {
+            return ID.CompareTo(((ItemDisc)o).ID);
+        }
+        else return 0;
+    }
 };
