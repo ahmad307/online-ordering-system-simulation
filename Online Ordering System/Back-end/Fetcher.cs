cf8e96e58dc98191f77e8e853d0619adc9314ae1
Ahmad <ahmad.hussain.307@gmail.com>
2017-06-08 15:57:08 +0200

    added username/password checker

--- Online Ordering System/Back-end/Fetcher.cs
+++ Online Ordering System/Back-end/Fetcher.cs
@@ -1,5 +1,6 @@
 ﻿/*Fetching Info from Database*/
 
+using System.Collections.Generic;
 
 namespace SQLCommunications
 {
@@ -29,6 +30,22 @@ namespace SQLCommunications
             }
             else return "Null";
 
+        }
+
+        public static List<ItemDisc> category(string s)
+        {
+            return new List<ItemDisc>(Receiver.ReadFromProduct("select * from Product where category=s"));
+        }
+
+        public static bool checkPass(string user, string pass)
+        {
+            User[] x = Receiver.ReadFromAccounts("select* from Accounts where username=user");
+            if (x.Length == 0) return false;
+            else if (pass == x[0].Password)
+            {
+                return true;
+            }
+            else return false;
         }
     }
 }
