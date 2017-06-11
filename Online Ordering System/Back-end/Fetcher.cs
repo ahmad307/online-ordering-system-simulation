8ca6f53e84ad6a08739890da2d5753d2c6210ba8
Ahmad <ahmad.hussain.307@gmail.com>
2017-06-12 01:18:12 +0200

    removed unused function

--- Online Ordering System/Back-end/Fetcher.cs
+++ Online Ordering System/Back-end/Fetcher.cs
@@ -9,28 +9,7 @@ public class FetchData
     {
         return Receiver.ReadFromProduct("SELECT * from Product WHERE name = '" + s + "';")[0];
     }
-
-    public static string GetProductDetail(string productName, string Detail)    //returns a string with a certain porperty of the product
-    {
-        float x;
-        if (Detail == "type")
-        {
-            return Receiver.ReadFromProduct("SELECT * FROM Product WHERE name = '" + productName + "';")[0].Type;
-        }
-        else if (Detail == "price")
-        {
-            x = Receiver.ReadFromProduct("SELECT * FROM Product WHERE name = '" + productName + "';")[0].price;
-            return x.ToString();
-        }
-        else if (Detail == "quantity")
-        {
-            x = Receiver.ReadFromProduct("SELECT * FROM Product WHERE name = '" + productName + "';")[0].Quantity;
-            return x.ToString();
-        }
-        else return "Null";
-
-    }
-
+    
     public static List<ItemDisc> Category(string s) //return a struct list of products in a certain category
     {
         return new List<ItemDisc>(Receiver.ReadFromProduct("SELECT * FROM Product WHERE category = '" + s + "';"));
